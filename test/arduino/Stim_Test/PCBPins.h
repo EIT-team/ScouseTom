b6dba01527bb92898100bb8dccecdc5a70cb4dd9
raquel-ucl <r.alegre@ucl.ac.uk>
2015-05-12 15:09:22 +0100

    Tests in Test_Arduino_Controller, minus the vcxproj, sln, suo files

---
+++ test/arduino/Stim_Test/PCBPins.h
@@ -0,0 +1,27 @@
+/*Pin Numbering for PCB version - changed to reduce trace length */
+
+// switch network pin definitions
+const int DINp = 39;
+const int DINn = 41;
+const int SCLK = 43;
+const int SYNC = 35;
+const int RESET = 37;
+
+// indicator pins
+const int IND_SWITCH = 5;
+const int IND_STIM = 6;
+const int IND_START = 7;
+const int IND_STOP = 8;
+const int IND_FREQ = 9;
+
+const int IND_EX_1= 10;
+const int IND_EX_2= 11;
+const int IND_EX_3= 12;
+
+
+//interupt pins
+const int INTR_PMARK = 25; // pin to decect pmark - MUST BE AT POSITION 0 OF A DUE PORT 25 is 0 on portD - check http://arduino.cc/en/Hacking/PinMappingSAM3X
+
+//Power Pins
+const int PWR_STIM = 52;
+const int PWR_SWITCH = 22;
