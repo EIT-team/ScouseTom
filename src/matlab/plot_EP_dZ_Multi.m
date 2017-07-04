933c873b5e1b00a9143f202c45b1d90d04687f77
keydawg <k.aristovich@ucl.ac.uk>
2017-07-04 17:48:32 +0100

    Changes made by Mayo and things

--- src/matlab/plot_EP_dZ_Multi.m
+++ src/matlab/plot_EP_dZ_Multi.m
@@ -1,3 +1,4 @@
+clear all
 HDR=ScouseTom_getHDR;
 
 %%
@@ -12,7 +13,7 @@ TT=ScouseTom_TrigProcess(Trigger,HDR);
 % becayse we use chn 5 as ref, we have to record all elecs 1-4.
 
 % map_ = [9:19 25];
-map_ = [ 7 8 9:18];
+map_ = [ 7 8 9:18 25];
 % map_ = [ 7 8 9 10 11];
 % map_ = [5 6 7  9:18];
 
@@ -132,11 +133,15 @@ xlabel('Time ms');
 ylabel('EP mV');
 title(sprintf('EP in file %s Stimulation %d',fname,iStim))
 % xlim([tau_max/2 - 10   tau_max])
-xlim([245 285])
+xlim([247.5 270])
 
- ylim([-20 10])
- 
- drawnow
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ylim([-20 5])
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+
+
+drawnow
 %   ylim([-40 40])
 %   
 % pause(0.00001);
@@ -168,7 +173,168 @@ set(gcf,'units','normalized','outerposition',[0 0 1 1])
 % xlim([245 285])
 % 
 %  ylim([-20 10])
+
+%%
+data = EP_avg(:,1)/1000;
+
+T0 = 248;
+T1 = T0 + 8;
+[~,idx1] = min(abs(T-T1));
+T2 = T0 + 16;
+[~,idx2] = min(abs(T-T2));
+T3 = T0 + 32;
+[~,idx3] = min(abs(T-T3));
+
+%Detect fisrt crossing of y = 0 after stimulation artefact this will be
+%start of first interval
+T_start = 251;
+t = find(T>T_start & T< T1);
+[~,idx_start] = min(abs(data(t)));
+idx_start = t(idx_start);
+T_start = T(idx_start);
+% 
+T_int = [T_start, T1, T2, T3];
+idx_int = [idx_start,idx1,idx2,idx3];
+idx_cross = [];
+%% Detect Area and Peaks for each interval 
+for iT = 1:length(T_int)-1
+    t = find(T > T_int(iT) & T< T_int(iT+1));
+    data_t = data(t);
+   
+    %this is really stupid. There must be a better way! Ask JAMEEEES?????
+    %Incase it crosses y axis during the interval
+    if any(data_t > 0)
+        for is = 1:length(data_t)
+            if data_t(is) < 0
+                sign(is) = -1;
+            else
+                sign(is) = 1;
+            end
+        end
+        diff_sign = diff(sign);
+        cross_point = find(diff_sign ~= 0);
+        if length(cross_point) <= 1
+            cross_idx = 1;
+            diff_mag = 1;
+        end
+        
+        if length(cross_point) > 1
+        diff_cross = diff(cross_point);
+        diff_mag = find(diff_cross > 20);
+               if isempty(diff_mag)
+            cross_idx = [1];
+            else
+            cross_idx = find(diff_cross>20);
+               end
+        end
+  
+        
+         
+        for ic = 1:(length(cross_idx)+1)
+            if ic == 1
+                cross_times(ic,:) = [1,cross_point(cross_idx(ic))];
+            elseif ic == length(cross_idx)+1
+                cross_times(ic,:) = [cross_point(cross_idx(ic-1)),length(data_t)];
+            else
+                cross_times(ic,:) = [cross_point(cross_idx(ic-1)),cross_point(cross_idx(ic))];
+            end
+        end
+        
+        
+   idx_c = unique(cross_times);
+   idx_c = t(idx_c);
+   t_cross = T(idx_c);
+   idx_cross = [idx_cross,idx_c];
+   
+      
+    for iC = 1:length(cross_times)
+        t_iC = t(cross_times(iC,1):cross_times(iC,2));
+        A(iC) = trapz(data_t(cross_times(iC,1):cross_times(iC,2)));
+        if A(iC) > 0
+            [P(iC),t_p(iC)] = max(data_t(cross_times(iC,1):cross_times(iC,2)));
+        else
+            [P(iC),t_p(iC)] = min(data_t(cross_times(iC,1):cross_times(iC,2)));
+        end
+        t_p(iC) = t_iC(t_p(iC));
+    end
+     
+    t_p = T(t_p);
+     
+     OUTPUT{iT}.A = A;
+     OUTPUT{iT}.P = P;
+     OUTPUT{iT}.T_c = t_cross;
+     OUTPUT{iT}.T_p = t_p;
+     OUTPUT{iT}.Interval = [T_int(iT),T_int(iT+1)];
+     
+     else
+       
+     A = trapz(data_t);
+     [P,t_p] = min(data_t);
+     t_p = T(t(t_p));
+     OUTPUT{iT}.A = A;
+     OUTPUT{iT}.P = P;
+     OUTPUT{iT}.T_p = t_p;
+     OUTPUT{iT}.Interval = [T_int(iT),T_int(iT+1)];
+    end
+end
+
+
+
+
+        
+%%
+
+%% Print out to screen
+n_str = {'First', 'Second', 'Third'};
+for iInt = 1:3
+n = length(OUTPUT{iInt}.A);
+fprintf('\n\n%s Interval T = %d to %d\n', n_str{iInt}, round(OUTPUT{iInt}.Interval(1,1)), round(OUTPUT{iInt}.Interval(1,2)));
+fprintf('No of peaks detected = %d\n', n);
+
+if n > 1
+    for iN = 1:n
+    fprintf('\nArea   A = %.0f mVms\n', OUTPUT{iInt}.A(iN));
+    fprintf('Peak   P = %.2f mV at T_p = %.2f ms\n', OUTPUT{iInt}.P(iN), OUTPUT{iInt}.T_p(iN));
+    end
+else
+    fprintf('Area   A = %.0f mVms\n', OUTPUT{iInt}.A);
+    fprintf('Peak   P = %.2f mV at T_p = %.2f ms\n', OUTPUT{iInt}.P, OUTPUT{iInt}.T_p);
+    
+end
+end
+
+   
+%%
+%Plot bar charts of area
+figure
+subplot(2,1,1)
+bar([OUTPUT{1}.A ,OUTPUT{2}.A, OUTPUT{3}.A]);
+ylabel('mVms');
+title('Area Under Curve');
+
+subplot(2,1,2)
+bar([OUTPUT{1}.P ,OUTPUT{2}.P, OUTPUT{3}.P]);
+ylabel('mV');
+title('Peak Value');
+
+
+figure; plot(T,data)
+hold on; plot([T(idx_start), T(idx_start)], [-20,5],'k')
+hold on; plot([T(idx1), T(idx1)], [-20,5], 'k')
+hold on; plot([T(idx2), T(idx2)], [-20,5], 'k')
+hold on; plot([T(idx3), T(idx3)], [-20,5],'k')
+for iP = 2:length(idx_cross)-1
+     hold on; plot([T(idx_cross(iP)), T(idx_cross(iP))], [-10,5],'k--')
+end
+hold on; plot([240,290], [0,0], 'k');
+xlim([247.5 285]);
+ylim([-20 5]);
+xlabel('Time ms');
+ylabel('EP mV');
+hold off;
  
+
+%clearvars -except AREA PEAK T_PEAK HDR FS fname Trigger TT map_ ref_chn Good_ch Data_demod iStim
 end
 
 
--- src/matlab/settings_single.m
+++ src/matlab/settings_single.m
@@ -1,19 +1,29 @@
-load('C:\Users\James\Documents\crab\Crab_09\Testing_log.mat')
+% this runs the set up of the current sources and arduino for a SINGLE
+% MEASUREMENT
+
+% ADD THIS TO PATH C:\Users\Anna\James\ScouseTom\src\matlab
+
+
+load('E:\Jimmy\Testing_log.mat')
 
 % run this first time
-[Ard]=ScouseTom_Init('COM6');
+[Ard]=ScouseTom_Init('COM18');
 
-AMP_RAMP = [ 500 1000 1500 2000 ]*1e-6;
-WIDTH_RAMP =  [ 2000 1000 500 1000 ];
+% this in ms so 30,000 is 30 seconds
 ExpSetup.MeasurementTime=30000;
+% this is in ms, and we want to set it to not a multiple of 50Hz 
+ExpSetup.StimulatorTriggerTime=988; % approx 1sec
 
-ExpSetup.CSStimAmp= 1000e-6; % this in in AMPS so you need e-6;
-ExpSetup.StimulatorPulseWidth  = 500;% this is in us
-ExpSetup.Freq = 1200;
-ExpSetup.Amp = 20;
+
+ExpSetup.CSStimAmp= 1500e-6; % this in in AMPS so you need e-6;
+ExpSetup.StimulatorPulseWidth  = 1000;% this is in us
+
+
+ExpSetup.Freq = 1200; % Hz
+ExpSetup.Amp = 20; %uA
 
 % send settings to controllers and current source
-[Ard,ExpSetup,isok]=ScouseTom_SendSettings_CS2(Ard,ExpSetup,'COM7');
+[Ard,ExpSetup,isok]=ScouseTom_SendSettings_CS2(Ard,ExpSetup,'COM17');
 
 % then you need to run this:
 % [ Ard,ExpSetup]=ScouseTom_Start(Ard,ExpSetup,1);
